0a30766048f20c0d89186d61f919c399ea69bc8e
AlexBugaev1990 <alex.bugaev90@gmail.com>
2022-09-12 18:29:52 +0300

    Добавьте файлы проекта.

---
+++ Practices.Tests/Class1.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practices.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(300, 10) == 290);
+        }
+
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Additional(290, 10) == 300);
+        }
+
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(30, 10) == 300);
+        }
+
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Division(300, 10) == 30);
+        }
+    }
+}
